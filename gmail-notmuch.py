fcbb3c682f1db68d973dcabed4ff4172abe6f407
Jason A. Donenfeld <Jason@zx2c4.com>
2012-12-07 00:46:34 +0100

    Add license.

--- gmail-notmuch.py
+++ gmail-notmuch.py
@@ -2,6 +2,11 @@
 
 # (C) Copyright 2012 Jason A. Donenfeld <Jason@zx2c4.com>. All Rights Reserved.
 
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+
 # ==============================
 # == Gmail-->Notmuch Importer ==
 # ==                          ==
