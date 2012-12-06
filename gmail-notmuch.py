0ed65047320eb625b6f8bd666b43104c37da9690
Jason A. Donenfeld <Jason@zx2c4.com>
2012-12-07 00:36:33 +0100

    Initial commit.

---
+++ gmail-notmuch.py
@@ -0,0 +1,166 @@
+#!/usr/bin/env python
+
+# (C) Copyright 2012 Jason A. Donenfeld <Jason@zx2c4.com>. All Rights Reserved.
+
+# ==============================
+# == Gmail-->Notmuch Importer ==
+# ==                          ==
+# ==     Work in progress.    ==
+# ==         by zx2c4         ==
+# ==                          ==
+# ==============================
+
+
+# $ ./gmail-notmuch.py -u jason.donenfeld -p Shien8Boh2vah ~/Mail/
+# Logging in...
+# Selecting all mail...
+# Collecting old messages...
+# Reading message list...
+# Downloading messages: 44 of 200|########                              | 22% ETA:  0:00:58 Elapsed Time: 0:00:16   2.66  emails/s
+
+# Interrupted imports will automatically resume from where they left off.
+
+from imaplib import IMAP4_SSL
+from optparse import OptionParser
+import sys
+import os.path
+import os
+import shlex
+import re
+import notmuch
+from progressbar import *
+
+def main():
+	parser = OptionParser(usage="%prog --username/-u USERNAME --password/-p PASSWORD --verbose/-v MAILDIR", description="Slurps gmail messages with labels into a notmuch maildir.")
+	parser.add_option("-u", "--username", action="store", type="string", metavar="USERNAME", help="Gmail username")
+	parser.add_option("-p", "--password", action="store", type="string", metavar="PASSWORD", help="Gmail password")
+	parser.add_option("-v", "--verbose", action="store_true", dest="verbose", default=False, help="Verbose output")
+	(options, args) = parser.parse_args()
+	if options.username == None or options.password == None:
+		parser.error("Username and password are required.")
+	if not options.username.lower().endswith("@gmail.com") and not options.username.lower().endswith("@googlemail.com"):
+		options.username += "@gmail.com"
+	if len(args) == 0:
+		parser.error("Maildir location is required.")
+	destination_dir = args[0]
+
+
+	imap = login(options)
+
+	print("Collecting old messages...")
+	old_messages = [os.path.basename(filename[0:filename.find(":")]) for filename in os.listdir(destination_dir + "/cur")]
+	
+	new_messages = discover_new_messages(imap, old_messages)
+	if len(new_messages) == 0:
+		print("Nothing to do!")
+		logout(imap)
+		sys.exit(0)
+	
+	download_new_messages(imap, new_messages, destination_dir)
+
+	logout(imap)
+
+def login(options):
+	print("Logging in...")
+	imap = IMAP4_SSL("imap.gmail.com")
+	if options.verbose:
+		imap.debug = 10
+	imap.login(options.username, options.password)
+	print("Selecting all mail...")
+	imap.select("[Gmail]/All Mail", True)
+	return imap
+
+def discover_new_messages(imap, old_messages):
+	print("Reading message list...")
+	typ, data = imap.fetch("1:*", "X-GM-MSGID")
+	new_messages = []
+	if typ != "OK":
+		sys.exit(("Failed to discover new messages: %s" % typ))
+	for response in data:
+		imap_seq = response[0:response.find(" ")]
+		gmail_id = response[response.rfind(" ") + 1:len(response) - 1]
+		if gmail_id in old_messages:
+			continue
+		new_messages.append((gmail_id, imap_seq))
+	return new_messages
+
+def download_new_messages(imap, messages, destination):
+	i=1
+	progressbar = ProgressBar(maxval=len(messages), widgets=["Downloading messages: ", SimpleProgress(), Bar(), Percentage(), " ", ETA(), " ", Timer(), " ", FileTransferSpeed(unit="emails")])
+	label_parser = re.compile(r'[0-9]+ [(]X-GM-LABELS [(](.*)[)] FLAGS [(](.*)[)][)]')
+	database = notmuch.Database(destination, False, notmuch.Database.MODE.READ_WRITE)
+
+	progressbar.start()
+	for gmail_id, imap_seq in messages:
+		typ, data = imap.fetch(str(imap_seq), "RFC822")
+		if typ != "OK":
+			sys.exit("Failed to download message gmail-%d/imap-%d" % (gmail_id, imap_seq))
+
+		temp = destination + "/tmp/" + str(gmail_id)
+		dest = destination + "/new/" + str(gmail_id)
+		f = open(temp, "w")
+		f.write(data[0][1])
+		f.close()
+
+		typ, data = imap.fetch(str(imap_seq), "(FLAGS X-GM-LABELS)")
+		if typ != "OK":
+			sys.exit("Failed to download labels gmail-%d/imap-%d" % (gmail_id, imap_seq))
+		
+		labels = label_parser.search(data[0]).groups()
+		labels = filter_labels(shlex.split(labels[0], False, True) + labels[1].split(" "))
+
+		os.link(temp, dest) # Because DJB says so...
+		os.unlink(temp)
+
+		message = database.add_message(dest, True)[0]
+		message.freeze()
+		for tag in labels:
+			message.add_tag(tag, True)
+		message.thaw()
+
+		progressbar.update(i)
+		i += 1
+	database.close()
+	progressbar.finish()
+
+def filter_labels(labels):
+	translation = {	"\\Inbox":	"inbox",
+			"\\Drafts":	"draft",
+			"\\Sent":	"sent",
+			"\\Spam":	"spam",
+			"\\Starred":	"flagged",
+			"\\Trash":	"deleted",
+			"\\Answered":	"replied",
+			"\\Flagged":	"flagged",
+			"\\Draft":	"draft",
+			"\\Deleted":	"deleted",
+			"\\Seen":	"!read!",
+			"\\Important":	None, # I realize this is controversial, but I hate the priority inbox.
+			"Junk":		"spam",
+			"NonJunk":	None }
+	ret = set()
+	for label in labels:
+		if label in translation:
+			if translation[label] is None:
+				continue
+			ret.add(translation[label])
+		else:
+			ret.add(label)
+	if "!read!" in ret:
+		ret.remove("!read!")
+	else:
+		ret.add("unread")
+	if "" in ret:
+		ret.remove("")
+	return ret
+			
+
+def logout(imap):
+	imap.close()
+	imap.logout()
+
+if __name__ == '__main__':
+	try:
+		main()
+	except KeyboardInterrupt:
+		sys.exit(1)
